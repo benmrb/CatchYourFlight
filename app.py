7675f9f16cd25aa959e40830ad364d65171786e6
benmrb <ben.marbacher@gmail.com>
2026-05-14 11:40:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import streamlit as st  # Streamlit importieren (Web-App-Framework)
 # Navigation mit mehreren Seiten konfigurieren
 pg = st.navigation(
     [
-        # Startseite: lädt pages/01_Dashboard.py, wird als Standard-Seite angezeigt
-        st.Page("pages/01_Dashboard.py",                title="Home",       default=True),
+        # Startseite: lädt 01_Dashboard.py, wird als Standard-Seite angezeigt
+        st.Page("01_Dashboard.py",                title="Home",       default=True),
         # Unterseite: lädt pages/02_Prediction.py
         st.Page("pages/02_Prediction.py", title="Prediction"),
     ],
